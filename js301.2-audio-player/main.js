82c83f3cf8e3759d3949507f21ee4c782a5499b7
Viktoriya Nykytenko <kitakiv@gmail.com>
2023-09-22 22:31:40 +0300

    feat: add progress and currentTime

--- js301.2-audio-player/main.js
+++ js301.2-audio-player/main.js
@@ -6,9 +6,18 @@ const player = document.getElementById('player'),
       background = document.querySelector('.background'),
       img = document.querySelector('.img'),
       nextButton = document.querySelector('.but-next'),
-      prewButton = document.querySelector('.but-prew')
-
-//
+      prewButton = document.querySelector('.but-prew'),
+      progressButton = document.querySelector('.progress'),
+      progressConteiner = document.querySelector('.progress-conteiner')
+//seconds
+const rightTime = (time) => (time < 10 ? `0${time}` : time)
+function toMinutes(seconds) {
+    let minutes = rightTime(Math.floor(seconds / 60));
+    // let min = Math.floor(seconds / 60)
+    let sec = rightTime(Math.floor(seconds - minutes * 60))
+    return `${minutes}:${sec}`
+}
+// play music
 const masOfTreks = ['Meliza', 'Hot girl bummer', 'This Year'];
 let index = 0;
 let isPlay = false;
@@ -24,19 +33,19 @@ function playMusic() {
         console.log(pauseOrPlay);
     }
 }
-
+// change the music
 function music() {
-    player.classList = `player${index + 1}`
     nameOfsong.innerHTML = masOfTreks[index];
     background.src = `./assets/img/cover${index + 1}.jpg`;
     img.src = `./assets/img/cover${index + 1}.jpg`;
     audio.src = `./assets/audio/song${index + 1}.mp3`
     pauseOrPlay.src = './assets/svg/pause.svg';
+    player.classList = `player${index + 1}`
     isPlay = true;
     audio.currentTime = 0;
     audio.play();
 }
-
+//next
 function nextSong() {
     if (index < masOfTreks.length - 1) {
         index += 1;
@@ -45,7 +54,7 @@ function nextSong() {
     }
     music()
 }
-
+//prew
 function prewSong() {
     if (index > 0) {
         index -= 1;
@@ -54,6 +63,27 @@ function prewSong() {
     }
     music()
 }
+
+// progress
+function progress(e) {
+    const {duration, currentTime} = e.srcElement;
+    document.querySelector('.time-finish').innerHTML = toMinutes(duration);
+    document.querySelector('.time-start').innerHTML = toMinutes(currentTime);
+    progressButton.style.width = `${currentTime / duration * 100}%`
+}
+// set progress
+function reaudio(event) {
+    const conteinerLong = this.clientWidth;
+    const clickX = event.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / conteinerLong) * duration;
+    pauseOrPlay.src = './assets/svg/pause.svg'
+    audio.play();
+    isPlay = true;
+}
+audio.addEventListener('ended', nextSong)
+progressConteiner.addEventListener('click', reaudio)
+audio.addEventListener('timeupdate', progress)
 nextButton.addEventListener('click', nextSong);
 buttonPlay.addEventListener('click', playMusic);
 prewButton.addEventListener('click', prewSong);
